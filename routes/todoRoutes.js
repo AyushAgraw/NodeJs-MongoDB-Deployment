bac87454d24527957c223a24669d5ae7765ff98f
Ayush Agrawal <ayushagrawalspr12345@gmail.com>
2025-12-13 19:15:41 +0530

    V3

---
+++ routes/todoRoutes.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const Todo = require("../models/Todo");
+
+
+const router = express.Router();
+
+
+// Create Todo
+router.post("/", async (req, res) => {
+try {
+const todo = await Todo.create(req.body);
+res.status(201).json(todo);
+} catch (err) {
+res.status(400).json({ error: err.message });
+}
+});
+
+
+// Get All Todos
+router.get("/", async (req, res) => {
+const todos = await Todo.find();
+res.json(todos);
+});
+
+
+// Update Todo
+router.put("/:id", async (req, res) => {
+const todo = await Todo.findByIdAndUpdate(req.params.id, req.body, { new: true });
+res.json(todo);
+});
+
+
+// Delete Todo
+router.delete("/:id", async (req, res) => {
+await Todo.findByIdAndDelete(req.params.id);
+res.json({ message: "Todo deleted" });
+});
+
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+
+const todoRoutes = require("./routes/todoRoutes");
+
+
+const app = express();
+app.use(express.json());
+
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.error(err));
+
+
+app.use("/api/todos", todoRoutes);
+
+
+app.listen(3000, () => {
+console.log("Server running on port 3000");
+});
