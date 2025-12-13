2e37c9635f589131b4255a15a94330045241d5eb
Ayush Agrawal <ayushagrawalspr12345@gmail.com>
2025-12-13 20:09:04 +0530

    adding index.html

--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -7,33 +7,33 @@ const router = express.Router();
 
 // Create Todo
 router.post("/", async (req, res) => {
-try {
-const todo = await Todo.create(req.body);
-res.status(201).json(todo);
-} catch (err) {
-res.status(400).json({ error: err.message });
-}
+    try {
+        const todo = await Todo.create(req.body);
+        res.status(201).json(todo);
+    } catch (err) {
+        res.status(400).json({ error: err.message });
+    }
 });
 
 
 // Get All Todos
 router.get("/", async (req, res) => {
-const todos = await Todo.find();
-res.json(todos);
+    const todos = await Todo.find();
+    res.json(todos);
 });
 
 
 // Update Todo
 router.put("/:id", async (req, res) => {
-const todo = await Todo.findByIdAndUpdate(req.params.id, req.body, { new: true });
-res.json(todo);
+    const todo = await Todo.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.json(todo);
 });
 
 
 // Delete Todo
 router.delete("/:id", async (req, res) => {
-await Todo.findByIdAndDelete(req.params.id);
-res.json({ message: "Todo deleted" });
+    await Todo.findByIdAndDelete(req.params.id);
+    res.json({ message: "Todo deleted" });
 });
 
 
--- server.js
+++ server.js
@@ -11,17 +11,17 @@ app.use(express.json());
 
 
 mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log("MongoDB Connected"))
-.catch(err => console.error(err));
+    .then(() => console.log("MongoDB Connected"))
+    .catch(err => console.error(err));
 
 
 app.use("/api/todos", todoRoutes);
 
 app.get("/", (req, res) => {
-  res.send("Welcome to your Node.js + MongoDB Todo app!");
+    res.send("Welcome to your Node.js + MongoDB Todo app!");
 });
 
 
 app.listen(3000, () => {
-console.log("Server running on port 3000");
+    console.log("Server running on port 3000");
 });
